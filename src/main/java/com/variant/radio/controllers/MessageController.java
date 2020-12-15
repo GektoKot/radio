a3ae449027c80e63a9695f0a96cdebb5515f72d3
Димас <52570598+GektoKot@users.noreply.github.com>
2020-12-15 11:13:39 +0100

    casual RestController

---
+++ src/main/java/com/variant/radio/controllers/MessageController.java
@@ -0,0 +1,63 @@
+package com.variant.radio.controllers;
+
+import com.variant.radio.exceptions.MessageNotFoundException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("messages")
+public class MessageController {
+
+    private static int counter = 4;
+
+    private List<Map<String, String>> messages = new ArrayList<Map<String, String>>() {{
+        add(new HashMap<String, String>() {{ put("id", "1"); put("text", "First message"); }});
+        add(new HashMap<String, String>() {{ put("id", "2"); put("text", "Second message"); }});
+        add(new HashMap<String, String>() {{ put("id", "3"); put("text", "Third message"); }});
+    }};
+
+    @GetMapping
+    public List<Map<String, String>> allMessages() {
+        return messages;
+    }
+
+    @GetMapping("{id}")
+    public Map<String, String> getOneMessage(@PathVariable String id) {
+        return getMessageFromDB(id);
+    }
+
+    @PostMapping
+    public Map<String, String> addNewMessage(@RequestBody Map<String, String> message) {
+        message.put("id", String.valueOf(counter++));
+
+        messages.add(message);
+
+        return  message;
+    }
+
+    @PutMapping("{id}")
+    public Map<String, String> updateMessage(@PathVariable String id, @RequestBody Map<String, String> message) {
+        Map<String, String> messageFromDB = getMessageFromDB(id);
+
+        messageFromDB.putAll(message);
+
+        return messageFromDB;
+    }
+
+    @DeleteMapping("id")
+    public void deleteMessage(@PathVariable String id) {
+        messages.remove(getMessageFromDB(id));
+    }
+
+    private Map<String, String> getMessageFromDB(String id) {
+        return messages.stream()
+                .filter(m -> m.get("id").equals(id))
+                .findFirst()
+                .orElseThrow(MessageNotFoundException::new);
+    }
+
+}
