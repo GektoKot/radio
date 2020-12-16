51f68837f8d51fd8ae79ec210f2a4c5cc1efc96f
Димас <52570598+GektoKot@users.noreply.github.com>
2020-12-16 13:52:18 +0100

    DB connected

--- src/main/java/com/variant/radio/controllers/MessageController.java
+++ src/main/java/com/variant/radio/controllers/MessageController.java
@@ -1,65 +1,65 @@
 package com.variant.radio.controllers;
 
-import com.variant.radio.exceptions.MessageNotFoundException;
+import com.fasterxml.jackson.annotation.JsonView;
+import com.variant.radio.domain.Message;
+import com.variant.radio.domain.Views;
+import com.variant.radio.repository.MessageRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("messages")
 public class MessageController {
 
-    private static int counter = 4;
+    private final MessageRepository messageRepository;
 
-    private List<Map<String, String>> messages = new ArrayList<Map<String, String>>() {{
-        add(new HashMap<String, String>() {{ put("id", "1"); put("text", "First message"); }});
-        add(new HashMap<String, String>() {{ put("id", "2"); put("text", "Second message"); }});
-        add(new HashMap<String, String>() {{ put("id", "3"); put("text", "Third message"); }});
-    }};
+    @Autowired
+    public MessageController(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
 
     @GetMapping
-    public List<Map<String, String>> allMessages() {
-        return messages;
+    @JsonView(Views.IdText.class)
+    public List<Message> allMessages() {
+        return messageRepository.findAll();
     }
 
     @GetMapping("{id}")
-    public Map<String, String> getOneMessage(@PathVariable String id) {
-        return getMessageFromDB(id);
+    public Message getOneMessage(@PathVariable("id") Message message) {
+        return message;
     }
 
     @PostMapping
-    public Map<String, String> addNewMessage(@RequestBody Map<String, String> message) {
-        message.put("id", String.valueOf(counter++));
-
-        messages.add(message);
-
-
-        return  message;
+    public Message addNewMessage(@RequestBody Message message) {
+        message.setDateOfCreation(LocalDateTime.now());
+        return  messageRepository.save(message);
     }
 
     @PutMapping("{id}")
-    public Map<String, String> updateMessage(@PathVariable String id, @RequestBody Map<String, String> message) {
-        Map<String, String> messageFromDB = getMessageFromDB(id);
-
-        messageFromDB.putAll(message);
-        messageFromDB.put("id", id);
-
-        return messageFromDB;
+    public Message updateMessage(@PathVariable("id") Message messageFromDB,
+                                 @RequestBody Message message) {
+        BeanUtils.copyProperties(message, messageFromDB, "id");
+        return messageRepository.save(messageFromDB);
+//        return messageRepository.save(message);
+//        messageFromDB.setText(message.getText());
+//        return messageRepository.save(messageFromDB);
     }
 
     @DeleteMapping("{id}")
-    public void deleteMessage(@PathVariable String id) {
-        messages.remove(getMessageFromDB(id));
+    public void deleteMessage(@PathVariable("id") Message message) {
+        messageRepository.delete(message);
     }
 
-    private Map<String, String> getMessageFromDB(String id) {
-        return messages.stream()
-                .filter(m -> m.get("id").equals(id))
-                .findFirst()
-                .orElseThrow(MessageNotFoundException::new);
-    }
+//    private Map<String, String> getMessageFromDB(String id) {
+//        return messages.stream()
+//                .filter(m -> m.get("id").equals(id))
+//                .findFirst()
+//                .orElseThrow(MessageNotFoundException::new);
+//    }
 
 }
